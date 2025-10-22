6f70cfcc09ba804449172185372fd3489a6aaa87
Basant-spec <basantdhiman9@gmail.com>
2025-10-22 11:12:25 +0530

    Initial commit by Basant-spec

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+app.use(express.json()); 
+const userRoutes = require('./view/user.routes');
+const productRoutes = require('./view/product.routes');
+const MONGO = 'mongodb+srv://basantdhiman9_db_user:<db_password>@ecommerce.06mvy46.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce'
+const cartRoutes = require("./view/cart.routes");
+const orderRoutes = require("./view/order.routes")
+
+const mongoose = require('mongoose');
+
+mongoose.connect(MONGO)
+.then(()=>{
+    console.log("DB connected");
+})
+.catch((e)=>{
+    console.log(e);
+})
+
+app.use("/user", userRoutes);
+app.use("/product", productRoutes);
+app.use('/cart', cartRoutes);
+app.use("/order", orderRoutes);
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("server started at 3000");
+})
